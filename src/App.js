16d049b76f2a89bc620af61f2b417d860b22bafa
pradipta18choudhury <pradipta1805@gmail.com>
2021-03-06 00:34:50 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
-import "./style.css";
+//import "./style.css";
 
-export default function App() {
+// functional component
+// we want that on clicking the name should alter
+function App()
+{
+  let item=1;
+  const name=["Pradipta", "Samrat"];
+  const handleclick= () => 
+  {
+    item=(item+1)%2;
+    console.log(item);
+  }
   return (
     <div>
-      <h1>Hello world</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+  <h1>Pradipta is Cool!!</h1>
+  <h1>Names:{name[item]}</h1>
+  <button onclick={handleclick}> click me </button>
+  </div>
   );
+  
 }
+//class component
+export class App2 extends React.Component
+{
+  render()
+  {
+    return <h1> Wat's up:)</h1>
+  }
+}
+export default App;
+
